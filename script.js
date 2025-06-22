acd3ea893feb522ad6c67d04c10546945a764c35
SHAHNOOR ISHTIYAQUE <148809310+Shahnoorishtiyaque@users.noreply.github.com>
2025-06-22 06:22:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -114,9 +114,9 @@ replayBTN.addEventListener("click", function () {
         box.innerHTML = ""
     })
     chance = true;
-    user1.style.backgroundColor = "transparent";
+    user1.style.backgroundColor = "white";
     user2.style.backgroundColor = "transparent";
-    user1.style.color="white";
+    user1.style.color="black";
     user2.style.color="white";
 
    // tictactoe.style.display = "none";
