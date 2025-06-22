b4abb459ef18566f51ca9d1283a9776f7a57023c
SHAHNOOR ISHTIYAQUE <148809310+Shahnoorishtiyaque@users.noreply.github.com>
2025-06-22 06:55:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -48,7 +48,6 @@ boxBTN.forEach((box, inex) => {
         } else {
             box.innerHTML = "O"
             box.style.color = "RED"
-            user1.style.backgroundColor="white
             user1.style.color="black";
             user2.style.backgroundColor = "transparent"
             user2.style.color="white"
